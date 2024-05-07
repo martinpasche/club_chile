749860166c3d58e91d4dcdd2ab3f06e998b3e59c
martinpasche <martin.pasche@student-cs.fr>
2024-05-07 23:35:12 +0200

    Update static and media file paths in settings.py

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from settings import STATIC_ROOT
+from django.conf import settings
 from whitenoise import WhiteNoise
 
 
@@ -19,5 +19,5 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
 application = get_wsgi_application()
 
-application = WhiteNoise(application, root=STATIC_ROOT)
-application.add_files(MEDIA_ROOT, prefix='media/')
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
+application.add_files(settings.MEDIA_ROOT, prefix='media/')
