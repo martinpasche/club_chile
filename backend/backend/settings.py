51cbcbe96ecaac484986bb49a7386e2d3bec098a
martinpasche <martin.pasche@student-cs.fr>
2024-05-08 19:26:38 +0200

    Update CSRF and session cookie attributes for improved security

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -190,8 +190,8 @@ else:
     # CSRF_COOKIE_HTTPONLY = True
     STATIC_ROOT = '/home/clubxkhj/public_html/static-django'
     MEDIA_ROOT = "/home/clubxkhj/public_html/media-django"
-    # CSRF_COOKIE_SAMESITE = "None"
-    # SESSION_COOKIE_SAMESITE = "None"
+    CSRF_COOKIE_SAMESITE = "None"
+    SESSION_COOKIE_SAMESITE = "None"
     CORS_ORIGIN_ALLOW_ALL = False
     
         
