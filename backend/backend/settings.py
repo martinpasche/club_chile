b09f33001b7cfb4247afbf844bc31629aaf9a298
martinpasche <martin.pasche@student-cs.fr>
2024-05-08 19:23:29 +0200

    Update CORS settings to allow all origins and credentials

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -184,15 +184,15 @@ else:
         "https://*.clubchilien.xyz",
     ]
     
-    CSRF_USE_SESSIONS = True
-    CSRF_COOKIE_SECURE = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_HTTPONLY = True
+    # CSRF_USE_SESSIONS = True
+    # CSRF_COOKIE_SECURE = True
+    # SESSION_COOKIE_SECURE = True
+    # CSRF_COOKIE_HTTPONLY = True
     STATIC_ROOT = '/home/clubxkhj/public_html/static-django'
     MEDIA_ROOT = "/home/clubxkhj/public_html/media-django"
-    CSRF_COOKIE_SAMESITE = "None"
-    SESSION_COOKIE_SAMESITE = "None"
-    CORS_ORIGIN_ALLOW_ALL = True
+    # CSRF_COOKIE_SAMESITE = "None"
+    # SESSION_COOKIE_SAMESITE = "None"
+    CORS_ORIGIN_ALLOW_ALL = False
     
         
     
