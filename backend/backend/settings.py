072a8169506472611d5f0d41b55bb76e6fbf43d6
martinpasche <martin.pasche@student-cs.fr>
2024-05-09 20:50:08 +0200

    chore: Update CSRF_COOKIE_HTTPONLY to False for practical security benefit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -243,7 +243,7 @@ else:
     #CSRF_USE_SESSIONS = True   #never use. it is used sometimes for better security, but i dont recommend
     CSRF_COOKIE_SECURE = True
     SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_HTTPONLY = True
+    CSRF_COOKIE_HTTPONLY = False   #setting this to true doesnt provide any practical security benefit
     SESSION_COOKIE_HTTPONLY = True
     CSRF_COOKIE_SAMESITE = "None"
     SESSION_COOKIE_SAMESITE = "None"
