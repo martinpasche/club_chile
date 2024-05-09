0fdf2c9ab2758275f98b4b0e04627f37fb551e3d
martinpasche <martin.pasche@student-cs.fr>
2024-05-09 12:26:10 +0200

    chore: Update axios defaults for CSRF token handling

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // axios.defaults.xsrfCookieName = 'csrftoken';
 //axios.defaults.xsrfHeaderName = 'X-CSRFTOKEN';
-axios.defaults.withCredentials = true;
+//axios.defaults.withCredentials = true;
 
 export default axios.create({
     baseURL: `https://django.clubchilien.xyz`,
