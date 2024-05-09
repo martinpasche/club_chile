09ad1802f2dcc205c711a29f03e3e5d7b2e8e3bc
martinpasche <martin.pasche@student-cs.fr>
2024-05-09 12:44:41 +0200

    chore: Update CSRF_COOKIE_NAME to "csrftoken" for improved security

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-// CSRF_COOKIE_NAME = "XSRF-TOKEN"
+
 // axios.defaults.xsrfCookieName = 'csrftoken';
 //axios.defaults.xsrfHeaderName = 'X-CSRFTOKEN';
 //axios.defaults.withCredentials = true;
 
 export default axios.create({
     baseURL: `https://django.clubchilien.xyz`,
-    withCredentials : true,
+    //withCredentials : true,
     //xsrfHeaderName: 'X-CSRFToken',
-    ///xsrfCookieName: 'csrftoken',
+    xsrfCookieName: 'csrftoken',
 });
